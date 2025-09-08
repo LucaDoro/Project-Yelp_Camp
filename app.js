188bf786077c4b834b5fd72a0d3f5b7bbad3da47
LucaDoro <luca.doro31@gmail.com>
2025-09-08 15:10:25 +0200

    deploying app

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const reviewRoutes = require("./routes/reviews");
 const MongoStore = require("connect-mongo");
 
 // const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
@@ -45,12 +45,12 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(sanitizeV5({ replaceWith: "_" }));
 
+const secret = process.env.SECRET || "thisshouldbeabettersecret";
+
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
-  crypto: {
-    secret: "secretword",
-  },
+  secret,
 });
 
 store.on("error", function (e) {
@@ -60,7 +60,7 @@ store.on("error", function (e) {
 const sessionConfig = {
   store,
   name: "session",
-  secret: "secretword",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
